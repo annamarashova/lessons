49438de0cd69223bac3f37396cd192d17969bdd1
Alexey <alex2304el@gmail.com>
2021-07-24 13:04:47 +0300

    списки

--- 012_списки.py
+++ 012_списки.py
@@ -1,10 +1,27 @@
-a = [1, 2, 3, 4, 5]
+a = [1, 2, 3, 4, 2, 5, 4]
 a.append(6)  # добавление значений
-a.remove(4)  # удаление значений
+a.remove(4)  # удаление значений - первого встретившегося
+# удаление всех двоек
+s = []
+for b in a:
+    if b == 2:
+        continue
+    s.append(b)
+print(s)
+print(list(reversed(s)))
+
+# обход и изменение списка с помощью цикла while
 n = 0
-while n != 5:
+while n != len(a):
     a[n] = a[n] ** 2
     n += 1
 print(a)
-a = [element ** 2 for element in a if element > 10]
-print(a)
+# обход исходного списка с сохранением измененных данных в новый
+# table = []
+# for b in a:
+#     b = b ** 3
+#     table.append(b)
+# print(table)
+# обход списка
+b = [element ** 2 for element in a if element > 10]
+print(b)
